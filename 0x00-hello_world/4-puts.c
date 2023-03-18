ef7885bc1bd01e10997caa5b32dc9cad0bb8c172
Mahmoud kamal <mahmoudkamalbe@gmail.com>
2023-03-18 09:43:26 +0200

    who tf uses terminal??"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main() {
-    char hello[] = "Programming is like building a multilingual puzzle";
+    char hello[] = "\"Programming is like building a multilingual puzzle";
     puts(hello);
     return 0;
 
