3254f781ea05762448f27e375d6f3db549163845
Mahmoud kamal <mahmoudkamalbe@gmail.com>
2023-03-18 09:54:18 +0200

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - block
  * return: 0
 */
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
