d2c46d7d6c0f8db1caf437ed809d7c2a9f7b821a
Mahmoud kamal <mahmoudkamalbe@gmail.com>
2023-03-24 07:17:50 +0200

    wtf am i doing?

--- 0x04-more_functions_nested_loops/0.isupper.c
+++ 0x04-more_functions_nested_loops/0.isupper.c
@@ -1,1 +1,11 @@
-This is home work part 5
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - main block
+ * Return: 0;
+*/
+
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z' );
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _isupper(int c);
+
+
+
+
+
+#endif
