5d1ac79fdfc6b4105a542c0c2d9ce67cc604c6c0
mahmoud <mahmoudkamalbe@gmail.com>
2023-03-30 06:00:32 +0200

    hugeee w

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -21,9 +21,9 @@ char *_strcat(char *dest, char *src)
     while (src[j] != '\0')
     {
         dest[i] = src[j];
-        j++;
+        i++;
         j++;
     }
-    dest[i] = "\0";
-    return(dest);
+    dest[i] = '\0';
+    return (dest);
 }
