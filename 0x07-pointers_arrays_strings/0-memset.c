bec42945e734b5db98468967d51c2353961e5b94
mahmoud <mahmoudkamalbe@gmail.com>
2023-04-03 14:47:43 +0200

    this is hella funny

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /***
- * _memset: Function that store a constent byte in a memory
+ * _memset - Function that store a constent byte in a memory
  * @s: The pointer where the memory is stored at
  * @b: The byte to fill with
  * @n: the number of bytes to fill with
+ * 
+ * Return: changed array to new constent byte value
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-char* p = s;
-while (n-- > 1)
-{
-    *p++ = b;
-}
-return (s);
-
+    int i = 0;
+    for ( ; n > 0; i++)
+    {
+        s[i] = b;
+        n--;
+    }
+    return(s);
 }
