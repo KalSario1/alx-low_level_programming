5726dbcfa1604bbc3dde2cc71001fffaaff05bfa
mahmoud <mahmoudkamalbe@gmail.com>
2023-04-06 12:16:53 +0200

    Task 4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int factorial - a function that returns the factorial of a given number
+ * @n: int number
+ * Return: 1 or 0
+*/
+
+int factorial(int n)
+{
+    if (n < 0)
+    {
+        return (0);
+    }
+    else
+    {
+        return (n * factorial(n-1));
+    }
+}
