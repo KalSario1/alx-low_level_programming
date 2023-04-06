829b88f7485aeb71a48987c640f6956963e899c9
mahmoud <mahmoudkamalbe@gmail.com>
2023-04-06 12:17:26 +0200

    Task 4 With Betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,12 +7,12 @@
 
 int factorial(int n)
 {
-    if (n < 0)
-    {
-        return (0);
-    }
-    else
-    {
-        return (n * factorial(n-1));
-    }
+if (n < 0)
+{
+return (0);
+}
+else
+{
+return (n * factorial(n-1));
+}
 }
