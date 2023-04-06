3393f813e54b198dbbd6c24fb797fcd11ec200c0
mahmoud <mahmoudkamalbe@gmail.com>
2023-04-06 12:06:08 +0200

    Task 3 COME ON

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,14 @@
 
 int _strlen_recursion(char *s)
 {
+    static int l = 0;
 if (*s == '\0')
 {
 return (0);
 }
 else
 {
-return (1); + _strlen_recursion(s + 1);
+l++;
+_strlen_recursion(++s);
 }
 }
