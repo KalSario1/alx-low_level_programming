522b773b8e499b28602cb792b78bbb19ad1205ec
mahmoud <mahmoudkamalbe@gmail.com>
2023-04-09 12:11:19 +0200

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - function that prints its name
+ * @argc: number of command lines
+ * @argv: an array
+ * Return: 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+printf("%s\n",argv[argc * 0]);
+return(0);
+}
