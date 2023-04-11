1a1187c783eb04c2365b784622ac1d39d2c99c89
mahmoud <mahmoudkamalbe@gmail.com>
2023-04-11 13:35:55 +0200

    okay

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that prints the number of arguments passed into it.
+ * @argc: number of command lines
+ * @argv: an array
+ * Return: 0
+*/
+
+int main(int argc, char *argv)
+{
+printf("%d\n", argc);
+return (0);
+}
