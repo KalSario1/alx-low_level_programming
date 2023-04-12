19a25a4c37c01d2544bed299d861c971f486146e
mahmoud <mahmoudkamalbe@gmail.com>
2023-04-12 06:39:58 +0200

    hello world

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,11 +8,9 @@
  * Return: 0
 */
 
-int main (int argc, char *argv[] _attribute__((unused)))
+int main (int argc, char *argv[])
 {
 for (int i= 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
+    printf("%s\n", argv[i]);
 return (0);
 }
