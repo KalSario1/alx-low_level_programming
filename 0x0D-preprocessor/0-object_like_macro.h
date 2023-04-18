7fe4698db2f24ac6ed3d8930fdab7a9fd66c2b8a
Mahmoud kamal <mahmoudkamalbe@gmail.com>
2023-04-18 13:08:41 +0200

    depression

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,8 +1,9 @@
-#ifndef OBJECT_LIKE_MACRO_
-#define OBJECT_LIKE_MACRO_
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 /**
  * File: 0-object_like_macro.h
- * Desc: Create a header file that defines a macro named SIZE as an abbreviation for the token 1024. 
+ * Desc: Create a header file that
+     defines a macro named SIZE as an abbreviation for the token 1024. 
 */
 
 #define SIZE 1024
