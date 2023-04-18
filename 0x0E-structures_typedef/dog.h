37b2430d673b4a03005dc3048ae897804d6d6bc7
Mahmoud kamal <mahmoudkamalbe@gmail.com>
2023-04-18 13:46:57 +0200

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,16 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+/**
+ * struct dog - structure for dog info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog owner
+*/
 struct dog 
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
 #endif
