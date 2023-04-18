7fb97b07da8033b9404849cd4ce66ad341a0c778
Mahmoud kamal <mahmoudkamalbe@gmail.com>
2023-04-18 13:06:49 +0200

    added desc for task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,9 @@
 #ifndef OBJECT_LIKE_MACRO_
 #define OBJECT_LIKE_MACRO_
+/**
+ * File: 0-object_like_macro.h
+ * Desc: Create a header file that defines a macro named SIZE as an abbreviation for the token 1024. 
+*/
 
 #define SIZE 1024
 
