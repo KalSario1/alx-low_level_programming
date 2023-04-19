7d15cc695cd6060ffc8b51407a81e885aea32fb4
mahmoud <mahmoudkamalbe@gmail.com>
2023-04-19 09:37:16 +0200

    man i love vim

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+* print_name - function that prints out name
+* @name:  name to be printed out
+* Return: Name
+*/
+
+
+void print_name(char* name, void (*f)(char*))
+{
+	return (*name);
+
+}
