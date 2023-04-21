4b764422e55e8ab8f29a36b48cec62464f0307a9
mahmoud <mahmoudkamalbe@gmail.com>
2023-04-21 08:00:40 +0200

    please

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,14 +15,14 @@ unsigned int i;
 
 if (n == 0)
 {
-    return (0);
+return (0);
 }
 
 va_start (args, n);
 
 for (i = 0; i < n; i++)
 {
-    sum += va_arg (args, int);
+sum += va_arg (args, int);
 }
 
 va_end (args);
