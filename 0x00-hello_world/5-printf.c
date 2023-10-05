22657f0b97d0cf85631d5b53d88b70315ca38486
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-05 14:01:46 +0300

    adjusted task 4 for betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
- * Return 0: True
+ * Return: 0
 */
 
 int main(void)
