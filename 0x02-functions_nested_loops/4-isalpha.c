be3a017d70d05fe9d6704d81e024c8380c8fb540
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-11 10:00:58 +0300

    task 4 coded

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if words that are undercase
+ * @c: the charcter checker
+ * Return: 1 if c is lowercase, 0 if otherwise
+*/
+
+int _isalpha(int c)
+{
+return (c >= 'a' && c <= 'Z');
+
+}
