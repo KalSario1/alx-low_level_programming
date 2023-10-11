0073c400b361614b9b3a0dce5cb0f8ff9ab5af14
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-11 09:20:35 +0300

    finished coding task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,24 @@
 #include "main.h"
+
+
+/**
+ * print_alphabet_x10 - function
+ * Return: Alphabet 10x lol
+*/
+
+
 void print_alphabet_x10(void)
 {
-    int i = 0;
-    while (i < 10)
-    {
-        for (char al = 'a'; al <= 'z'; al++)
-        {
-            _putchar(al);
-        }
-        _putchar('\n');
-        i++;    }
-    return (0);
+int i = 0;
+while (i < 10)
+{
+char al;
+for (al = 'a'; al <= 'z'; al++)
+{
+_putchar(al);
+}
+_putchar('\n');
+i++;
+}
+return (0);
 }
