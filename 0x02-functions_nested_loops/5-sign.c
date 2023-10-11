5841b2a5eece2734b7a034dde847760fbbf730a6
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-11 10:54:36 +0300

    fixed task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,7 +13,7 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-else if (n = 0)
+else if (n == 0)
 {
 _putchar('0');
 return (0);
@@ -21,6 +21,7 @@ return (0);
 else if (n < 0)
 {
 _putchar ('-');
-return (-1);
+
 }
+return (-1);
 }
