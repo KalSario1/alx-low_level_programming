ee768b5d74bbb7bafd874c8ee38238d5cb72c07d
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-11 11:09:53 +0300

    i feel lathargic af

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _isalpha - check if words that are undercase
- * @c: the charcter checker
+ * _abs - check if words that are undercase
+ * @n: the charcter checker
  * Return: 1 if c is lowercase, 0 if otherwise
 */
 
 int _abs(int n)
 {
-if (n >= 0 && n <0)
+if (n >= 0)
 {
-    return (abs(n));
+    return (n);
 }
+    return (-n);
 }
