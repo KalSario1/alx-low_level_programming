b4928cbaf04b00eda6788cc0c67c71ee74dbed3d
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-11 11:13:19 +0300

    ya rab make this code work

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check if words that are undercase
+ * @n: the charcter checker
+ * Return: 1 if c is lowercase, 0 if otherwise
+*/
+
+int print_last_digit(int n)
+{
+return (n % 10);
+}
