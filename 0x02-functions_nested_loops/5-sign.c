581581a7f2e0c58167860bf6f69e75470eaa093c
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-11 10:55:20 +0300

    doctument error

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_sign- check if words that are undercase
+ * print_sign - check if words that are undercase
  * @n: the charcter checker
  * Return: 1 if c is lowercase, 0 if otherwise
 */
