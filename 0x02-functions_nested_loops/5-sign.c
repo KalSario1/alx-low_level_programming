36eadae1ae9555c15a12e70e54512fd0a7c8551d
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-11 10:52:55 +0300

    finished task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _print_sign- check if words that are undercase
+ * @n: the charcter checker
+ * Return: 1 if c is lowercase, 0 if otherwise
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n = 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+_putchar ('-');
+return (-1);
+}
+}
