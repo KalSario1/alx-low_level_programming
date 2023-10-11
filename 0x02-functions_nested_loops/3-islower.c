e212f3cf8eb144da5151af3bc8cbf5abad07d534
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-11 09:58:24 +0300

    edited the docs

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,12 @@
 
 /**
  * _islower - check if words that are undercase
+ * @c: the charcter checker
  * Return: 1 if c is lowercase, 0 if otherwise
 */
 
 int _islower(int c)
 {
 return (c >= 'a' && c <= 'z');
+
 }
