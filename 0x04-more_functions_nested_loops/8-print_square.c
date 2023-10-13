6f236c83744ad04b8c47fc87ea90175ebfab8f5e
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-13 21:21:23 +0300

    task 8 coded

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - function that return underscore
+ * @size: int checker
+ * Return: True or False
+*/
+
+
+void print_square(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
+for (i = 0; i < size; i++)
+{
+while (size > 0)
+{
+_putchar('#');
+size;
+}
+}
+}
+}
