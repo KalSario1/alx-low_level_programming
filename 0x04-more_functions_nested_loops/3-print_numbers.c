8f6feee30ad6ca4f07238a01b636ad0f5dab3f97
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-13 09:48:56 +0300

    task 3 done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function count to 10
+ * Return: True or False
+*/
+
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar (i);
+} 
+_putchar ("\n");
+}
