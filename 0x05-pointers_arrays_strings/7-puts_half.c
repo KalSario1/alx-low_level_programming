a320fdf269e60753b984ae2852b9c9106421ac83
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-17 00:22:31 +0300

    done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - function
+ * @str: string
+ * Return: Void
+*/
+
+void puts_half(char *str)
+{
+int len = 0;
+int start;
+while (str[len] != '\0')
+{
+len++;
+}
+if (len % 2 == 0)
+{
+start = len / 2;
+}
+else
+{
+start = (len - 1) / 2;
+}
+for (int i= start; i < len; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
