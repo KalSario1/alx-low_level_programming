7ac5cf4657c102c7ec3ca83975786e8a613c857f
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-17 00:00:18 +0300

    woah

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/***
+ * rev_string - function
+ * @s: string
+ * Return: Void
+*/
+
+void rev_string(char *s)
+{
+int len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+int start = 0;
+int end = len - 1;
+int temp;
+while (start > end)
+{
+temp = s[start];
+s[start] = s[end];
+s[end] = temp;
+
+start++;
+end--;
+}
+}
