ebf80d87a196bf5c97f2349420e7972c1986de3b
KalSario1 <mahmoudkamalbe@gmail.com>
2023-10-26 04:19:46 +0300

    finished coding task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function
+ * @n: integer
+ * Return: int
+*/
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0 || n == 1)
+{
+return (1);
+}
+
+}
