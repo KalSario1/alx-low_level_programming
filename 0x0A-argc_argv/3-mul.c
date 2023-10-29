75c7145639b43e2ccf4a957154ab0facf93edaef
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-29 13:27:26 +0200

    finished coding

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,49 +1,40 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * custom_atoi - converts a string to an integer
+ * @str: string to be converted
  *
- * Return: the int converted from the string
+ * Return: the integer converted from the string
  */
-int _atoi(char *s)
+int custom_atoi(char *str)
 {
-	int i, d, n, len, f, digit;
+    int i, sign, result;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+    i = 0;
+    sign = 1;
+    result = 0;
 
-	while (s[len] != '\0')
-		len++;
+    if (str[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
 
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
+    while (str[i] != '\0')
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            result = result * 10 + (str[i] - '0');
+        }
+        else
+        {
+            // Handle invalid characters in the string
+            return 0;
+        }
+        i++;
+    }
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+    return result * sign;
 }
 
 /**
@@ -55,19 +46,26 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+    int result, num1, num2;
 
-	if (argc < 3 || argc > 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return 1;
+    }
 
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
+    num1 = custom_atoi(argv[1]);
+    num2 = custom_atoi(argv[2]);
 
-	printf("%d\n", result);
+    if (num1 == 0 || num2 == 0)
+    {
+        printf("Error\n");
+        return 1;
+    }
 
-	return (0);
+    result = num1 * num2;
+
+    printf("%d\n", result);
+
+    return 0;
 }
