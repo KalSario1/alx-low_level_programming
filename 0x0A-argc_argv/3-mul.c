7d46aa653066712de24c6a749cd02e6f149efbfe
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-29 12:26:45 +0200

    fixed the code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,8 +14,8 @@ if (agrc != 3)
 printf("Error\n");
 return (1);
 } 
-int num1 = argv[1];
-int num2 = argv[2];
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
 
 int results = num1 * num2;
 return (0);
