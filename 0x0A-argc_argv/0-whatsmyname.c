17da93b9a6d983e240ee3d465677fd540e3e637f
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-29 11:14:41 +0200

    finished coding

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Int or 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+for (int i = 0; i < argv; i++ )
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
