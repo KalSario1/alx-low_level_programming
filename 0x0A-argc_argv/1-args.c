0ac84a9f7abc0875c63567ebf2e904c971bb46e1
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-29 11:22:20 +0200

    at least im trying

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Int or 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%d\n", argc[argv]);
+}
+return (0);
+}
