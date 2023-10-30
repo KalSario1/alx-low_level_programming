3f1cab50efe28cf2b172292d53fe1dab33674f90
mahmoud <mahmoudkamalbe@gmail.com>
2023-10-30 21:26:03 +0200

    coding in vim is weird

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strdup - function
+ * @str: string
+ * Return: NULL or CHAR
+ */
+
+char *_strdup(char *str)
+{
+	char *new_str;
+	size_t i;
+	if (str == NULL)
+		return NULL;
+	size_t length = 0;
+	while (str[length] != '\0')
+			length++;
+		new_str = (char *)malloc(length + 1);
+		if (new_str != NULL)
+			for (i = 0; i < length; i++)
+				new_str[i] = str[i];
+			new_str[i] = str[i];
+	return new_str;
+}
