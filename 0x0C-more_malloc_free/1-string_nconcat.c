23f0c1b5e79b9b656fc8aa8606c4f92fcfdbf96b
mahmoud <mahmoudkamalbe@gmail.com>
2023-11-03 22:10:31 +0200

    betty coding is hard af

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,34 +10,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *result;
-	unsigned int len1 = 0, len2 = 0, result_len, i,j;
+char *result;
+unsigned int len1 = 0, len2 = 0, result_len, i,j;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-	while (s1[len1] != '\0')
-			len1++;
-	while (s2[len2] != '\0')
-			len2++;
-	if (n >= len2)
-		n = len2;
-	result_len = len1 + n;
-	result =(char *)malloc(result_len + 1);
-
-	if (result == NULL)
-		return (NULL);
-
-	for (i = 0; i < len1; i++)
-	{
-		result[i] = s1[i];
-	}
-	for (j = 0; j < n; j++)
-	{
-		result[i++] = s2[j];
-	}
-	result[i] = '\0';
-	return (result);
+while (s1[len1] != '\0')
+{
+len1++;
+}
+while (s2[len2] != '\0')
+len2++;
+if (n >= len2)
+n = len2;
+result_len = len1 + n;
+result =(char *)malloc(result_len + 1);
+if (result == NULL)
+return (NULL);
+for (i = 0; i < len1; i++)
+{
+result[i] = s1[i];
+}
+for (j = 0; j < n; j++)
+{
+result[i++] = s2[j];
+}
+result[i] = '\0';
+return (result);
 }
